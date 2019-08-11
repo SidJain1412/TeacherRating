bdaabc3180c2b3a89f21a67865692061398e187f
Siddharth <sidjain1412@gmail.com>
2019-08-12 01:14:41 +0530

    Fixed forms a little bit

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,24 @@
 # Using Flask WTF (WT Forms) to represent web forms
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField('Sign In')
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Name', validators=[DataRequired()])
     # Automatic resolving of email
     email = StringField('Email', validators=[DataRequired(), Email()])
-    dept = StringField('Department', validators=[DataRequired()])
+    dept = SelectField('Department', choices=[(
+        'cse', 'CSE'), ('it', 'IT'), ('swe', 'Software')], validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     # Check if both passwords are the same
     password2 = PasswordField('Repeat Password', validators=[
