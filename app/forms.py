e35de9bc73b9484bf99e545d56bedcd1cc04b76f
Siddharth <sidjain1412@gmail.com>
2019-08-12 00:09:14 +0530

    Forms file

---
+++ app/forms.py
@@ -0,0 +1,6 @@
+# Using Flask WTF (WT Forms) to represent web forms
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from app.models import User
