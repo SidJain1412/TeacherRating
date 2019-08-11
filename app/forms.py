33e395a7d0411940658b1b495a989ee643f36f7b
Siddharth <sidjain1412@gmail.com>
2019-08-12 00:54:20 +0530

    Forms added for login and registration

--- app/forms.py
+++ app/forms.py
@@ -4,3 +4,27 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    # Automatic resolving of email
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    dept = StringField('Department', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    # Check if both passwords are the same
+    password2 = PasswordField('Repeat Password', validators=[
+                              DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('E-mail is already in use. Try again.')
